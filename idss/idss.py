e4fdfd951e51cb157816543381768a28f3d57828
Steve Kowalik <steven@wedontsleep.org>
2025-12-30 18:13:59 +1100

    Comment out POC code, fix lint

--- idss/idss.py
+++ idss/idss.py
@@ -30,15 +30,14 @@ class File:
         pass
 
 
-
 class IDSS:
     def __init__(self):
-        MASK = constants.IN_MOVE | constants.IN_DELETE
+        mask = constants.IN_MOVE | constants.IN_DELETE
         self.event_map = self.generate_event_map()
         self.maildir = pathlib.Path.home() / "Maildir"
         self.deliveries = Deliveries()
         self.i = adapters.Inotify()
-        self.i.add_watch(str(maildir / ".Spam" / "cur"), mask=MASK)
+        self.i.add_watch(str(self.maildir / ".Spam" / "cur"), mask=mask)
 
     def generate_event_map(self):
         event_map = {}
@@ -52,42 +51,46 @@ class IDSS:
     def process_events(self):
         for event in self.i.event_gen(yield_nones=False):
             (_, event_type, _, filename) = event
-            logging.debug("Calling {event_map[event_type]} for {filename}")
-            getattr(self, event_map[event_type])(filename)
+            logging.debug(
+                "Calling {self.event_map[event_type]} for {filename}"
+            )
+            if event_type not in self.event_type:
+                logging.debug(
+                    f"Unhandled EVENT={event_type}, FILENAME={filename}"
+                )
+                continue
+            getattr(self, self.event_map[event_type])(filename)
 
     def delete_event(self, filename):
-        if "IN_DELETE" in event_type:
-            # has it been moved out?
-            if self.maildir.rglob(filename):
-                self.train(filename, "ham")
+        # has it been moved out?
+        if self.maildir.rglob(filename):
+            self.train(filename, "ham")
 
-        # unhandled event, fallback
-        #logging.debug(f"Unhandled EVENT={event_type}, FILENAME={filename}")
 
-def idss():
-    deliveries = Deliveries()
-    i = adapters.Inotify()
-    for event in i.event_gen(yield_nones=False):
-        filename = File(filepath)
-        if "IN_MOVED_FROM" in event_type:
-            if filename.flags is None:
-                deliveries.new(filename)
-                continue
-        if "IN_MOVED_TO" in event_type:
-            # fresh delivery, therefore no flags
-            if filename.endswith(","):
-                logging.debug(f"Fresh message {filename}")
-                deliveries.append(filename)
-                continue
-            if filename.endswith(",S"):
-                # now seen, is it a delivery?
-                logging.debug(f"Seen message {filename}")
-                if filename[:-1] in deliveries:
-                    logging.debug("In deliveries, removing")
-                    deliveries.remove(filename[:-1])
-                    continue
-                train(filename)
-                # not a delivery, it's been moved into spam
-                logging.debug(f"Train {filename} as spam")
-                continue
-
+# def idss():
+#    deliveries = Deliveries()
+#    i = adapters.Inotify()
+#    for event in i.event_gen(yield_nones=False):
+#        filename = File(filepath)
+#        if "IN_MOVED_FROM" in event_type:
+#            if filename.flags is None:
+#                deliveries.new(filename)
+#                continue
+#        if "IN_MOVED_TO" in event_type:
+#            # fresh delivery, therefore no flags
+#            if filename.endswith(","):
+#                logging.debug(f"Fresh message {filename}")
+#                deliveries.append(filename)
+#                continue
+#            if filename.endswith(",S"):
+#                # now seen, is it a delivery?
+#                logging.debug(f"Seen message {filename}")
+#                if filename[:-1] in deliveries:
+#                    logging.debug("In deliveries, removing")
+#                    deliveries.remove(filename[:-1])
+#                    continue
+#                train(filename)
+#                # not a delivery, it's been moved into spam
+#                logging.debug(f"Train {filename} as spam")
+#                continue
+#
